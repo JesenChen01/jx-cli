a064c88d4303dbbf3d229b7ca354368a86b172aa
Jesen <jesenchen01@gmail.com>
2023-08-27 10:03:37 +0800

    开发命令行工具

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jx-cli
+
 拉取个人的项目模版
+
+node bin create <project-name>
---
+++ bin/init.js
@@ -0,0 +1,38 @@
+/**
+ * 项目克隆
+ */
+let { promisify } = require("util");
+const ora = require("ora");
+const download = promisify(require("download-git-repo"));
+const shell = require("shelljs");
+let chalk = require("chalk");
+
+// 日志打印函数
+const log = (content) => console.log(chalk.yellow(content));
+
+module.exports = async (appName) => {
+  log(`🚀 创建项目 ${appName}`);
+  shell.rm("-rf", appName);
+  const spinner = ora("下载中...").start();
+  try {
+    await download(
+      "direct:https://github.com/JesenChen01/vite-vue-general-cli.git#main",
+      appName,
+      { clone: true }
+    );
+    spinner.succeed("下载完成");
+    log(`
+下载完成，请执行下面命令启动项目：
+===========================
+cd ${appName}
+yarn 或者 npm init 
+
+npm run dev
+或者
+yarn dev
+        `);
+  } catch (error) {
+    log(`下载失败`, error);
+    spinner.stop();
+  }
+};
